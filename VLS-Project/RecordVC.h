1115913c5cceddbe89418f8a3e80d13ef55b3705
Ryan DuToit <Ryan.DuToit@drake.edu>
2011-06-08 11:37:16 -0500

    test comment

--- VLS-Project/RecordVC.h
+++ VLS-Project/RecordVC.h
@@ -4,7 +4,7 @@
 //
 //  Created by Ryan Dutoit on 6/7/11.
 //  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
+// testing change
 
 #import <UIKit/UIKit.h>
 #import <AVFoundation/AVFoundation.h>
